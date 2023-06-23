7f72d42b08aa67e0c1046facd49202e2ecbb1e6c
dg1223 <alavi1223@hotmail.com>
2023-06-23 14:25:03 +0600

    refactor: display raw data

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -24,12 +24,14 @@ def validate_input(user_input, valid_values, value_dict, prompt):
     Returns:
         str: The validated user input.
     """
+
     while user_input.lower() not in valid_values:
         print("\n")
         print(f"Invalid input. Please enter a valid value as instructed.")
         print("\n")
         user_input = input(prompt)
 
+    user_input = user_input.lower()
     print("\n")
     chosen_value = value_dict.get(user_input, "Invalid value")
     print("You have chosen:", chosen_value)
@@ -58,7 +60,7 @@ def get_filters():
         "washington": "washington",
     }
 
-    city_prompt = "Enter the name of the city you want to analyze. Choose any of these 3 cities: chicago, new york, washington. Please enter the name exactly as shown.: "
+    city_prompt = "Enter the name of the city you want to analyze. Choose any of these 3 cities: chicago, new york, washington.: "
 
     city = validate_input(input(city_prompt), valid_cities, city_dict, city_prompt)
 
@@ -217,34 +219,42 @@ def load_data(city, month, day):
     return df
 
 
-def time_stats(df):
+def time_stats(df, month, day):
     """Displays statistics on the most frequent times of travel."""
 
     print("\nCalculating The Most Frequent Times of Travel...\n")
     start_time = time.time()
 
     # display the most common month
-
-    # Extract month from Start Time column
     df["Month"] = df["Start Time"].dt.month
-    # Find the most common month
     common_month = df["Month"].mode()[0]
-    # Convert month number to month name
     common_month_name = calendar.month_name[common_month]
-    print("The most common month: ", common_month_name)
+
+    if month != "all":
+        print(
+            "The most common month is '{}' because this is the only month chosen as a filter".format(
+                common_month_name
+            )
+        )
+    else:
+        print("The most common month:", common_month_name)
 
     # display the most common day of week
     df["Day of Week"] = df["Start Time"].dt.dayofweek
-    # Find the most common day of week
     common_day = df["Day of Week"].mode()[0]
-    # Convert day number to day name
     common_day_name = calendar.day_name[common_day]
-    print("The most common day of week: ", common_day_name)
+
+    if day != "all":
+        print(
+            "The most common day of week is '{}' because this is the only day chosen as a filter".format(
+                common_day_name
+            )
+        )
+    else:
+        print("The most common day of week:", common_day_name)
 
     # display the most common start hour
-    # Extract hour from Start Time column
     df["Hour"] = df["Start Time"].dt.hour
-    # Find the most common start hour
     common_hour = df["Hour"].mode()[0]
     print("The most common start hour: ", common_hour)
 
@@ -358,7 +368,26 @@ def main():
 
         # handle exception when no data is available
         if df.size > 0:
-            time_stats(df)
+            # if there is data, ask if the user wants to see raw data first
+            show_raw_data = input(
+                "\nWould you like to see 5 lines of raw data? Enter yes or no.\n"
+            )
+            show_raw_data = show_raw_data.lower()
+            start_row = 0
+            while show_raw_data == "yes":
+                print("\n")
+                print(df.iloc[start_row : start_row + 5])
+                start_row += 5
+                show_raw_data = input(
+                    "\nWould you like to see the next 5 lines of raw data? Enter yes or no.\n"
+                )
+                show_raw_data = show_raw_data.lower()
+                if start_row >= len(df) or show_raw_data != "yes":
+                    break
+
+            # show stats
+            # if df.size > 0:
+            time_stats(df, month, day)
             station_stats(df)
             trip_duration_stats(df)
             user_stats(df)
